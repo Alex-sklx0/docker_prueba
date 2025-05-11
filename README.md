91dbc3cabf5b71205c39ca285e64788ebf864c97
Ubuntu <ubuntu@ip-172-31-28-211.ec2.internal>
2025-05-11 16:53:16 +0000

    flaskmejoradoYreadme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # docker_prueba
+# Smashing Pumpkins
+
+[![Docker](https://img.shields.io/badge/Docker-Enabled-blue.svg)](https://www.docker.com/)
+[![Flask](https://img.shields.io/badge/Framework-Flask-green.svg)](https://flask.palletsprojects.com/)
+
+Sitio web fanático de The Smashing Pumpkins implementado con Flask y Docker.
+
+## 🚀 Despliegue Rápido
+
+### Requisitos
+- Docker instalado
+- Git (opcional)
+
+### Instalación
+```bash
+git clone https://github.com/tuusuario/smashing-pumpkins-fan-site.git
+cd smashing-pumpkins-fan-site
+docker build -t pumpkins-site .
+docker run -d -p 80:5000 --name pumpkins_app pumpkins-site
--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
-# app/main.py
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "¡Servicio telemático funcionando!"
+# Datos de la banda
+band_info = {
+    "name": "The Smashing Pumpkins",
+    "genres": ["Alternative rock", "Shoegaze", "Dream pop"],
+    "year_formed": 1988,
+    "members": [
+        {"name": "Billy Corgan", "role": "Vocals, Guitar"},
+        {"name": "James Iha", "role": "Guitar"},
+        {"name": "Jimmy Chamberlin", "role": "Drums"},
+        {"name": "D'arcy Wretzky", "role": "Bass"}
+    ],
+    "albums": [
+        {"name": "Siamese Dream", "year": 1993},
+        {"name": "Mellon Collie and the Infinite Sadness", "year": 1995},
+        {"name": "Adore", "year": 1998}
+    ]
+}
 
-@app.route('/api/data')
-def get_data():
-    return {"message": "Datos del servicio", "status": "ok"}
+@app.route('/')
+def index():
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', 
+                         band=band_info,
+                         current_year=current_year)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
